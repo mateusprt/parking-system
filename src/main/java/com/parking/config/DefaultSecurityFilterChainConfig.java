fe1586f696431d67d179df4aaae96b587d6d602e
mateusprt <matnun95@gmail.com>
2023-05-15 10:22:56 -0300

    Set cors configuration

--- src/main/java/com/parking/config/DefaultSecurityFilterChainConfig.java
+++ src/main/java/com/parking/config/DefaultSecurityFilterChainConfig.java
@@ -1,15 +1,20 @@
 package com.parking.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +29,21 @@ public class DefaultSecurityFilterChainConfig {
 		http
 			.csrf()
 			.disable()
+			.cors()
+			.configurationSource(new CorsConfigurationSource() {
+				
+				@Override
+				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+					CorsConfiguration config = new CorsConfiguration();
+					config.setAllowedOrigins(List.of("*"));
+					config.setAllowedMethods(List.of("*"));
+					config.setAllowCredentials(true);
+					config.setAllowedHeaders(List.of("Authorization"));
+					config.setMaxAge(3600L);
+					return config;
+				}
+			})
+			.and()
 			.authorizeHttpRequests()
 			.requestMatchers("/api/v1/auth/**")
 			.permitAll()
